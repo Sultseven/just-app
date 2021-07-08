927154cf2286e99fb26e7872f17a6102a707fc1b
Sultcon1 <sult.seven17@mail.ru>
2021-07-08 21:30:55 +0300

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,23 @@
+import React, {Fragment} from 'react'
+import {NavLink} from 'react-router-dom'
+
+export const Navbar = () =>{
+	return(
+		<nav className="navbar navbar-dark navbar-expand-lg bg-primary">
+			<div className="container">
+				<div className="navbar-brand">
+					Note App
+				</div>
+				<ul className="navbar-nav">
+			        <li className="nav-item">
+			        	<NavLink className="nav-link active" to="/">Home</NavLink>
+			        </li>
+			        <li className="nav-item">
+			        	<NavLink className="nav-link" to="/about">About</NavLink>
+			        </li>
+		      	</ul>
+			</div>
+		</nav>
+	)
+	//нету функциональности, поэтому "return" можно не писать(можно стереть)
+}
---
+++ src/pages/About.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+export const About = () =>{
+	return(
+		<div class="jumbotron">
+		  <div class="container">
+		    <h1 class="display-3">React app</h1>
+		    <p class="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
+		  </div>
+		</div>
+	);
+}
